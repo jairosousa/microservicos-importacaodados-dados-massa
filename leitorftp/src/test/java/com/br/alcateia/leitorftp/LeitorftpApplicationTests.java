a0574fda8e0869ba5124bb358f608de5259c60b1
Jairo <jaironsousa@gmail.com>
2021-06-06 17:27:08 -0300

    :fire: Criação servidor ftp e api leitor-ftp

---
+++ leitorftp/src/test/java/com/br/alcateia/leitorftp/LeitorftpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alcateia.leitorftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitorftpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
